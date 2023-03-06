8d3852611364280cdd580ec3de0cc78f3d334c1a
Alejandragomez29 <123989655+Alejandragomez29@users.noreply.github.com>
2023-03-06 10:40:43 -0300

    Add files via upload

---
+++ js/function.js
@@ -0,0 +1,16 @@
+document.getElementById('boton').onclick = function (){
+    console.log("Datos personales");
+    document.getElementById("demo").innerHTML = "Camino del peru 150"
+}
+document.getElementById('boton2').onclick = function (){
+    console.log("Email");
+    document.getElementById("demo").innerHTML = "gomez-21alejandra@hotmail.com"
+}
+document.getElementById('boton3').onclick = function(){
+    console.log("Telefono");
+    document.getElementById("demo").innerHTML= "3815478960"
+}
+document.getElementById('boton4').onclick= function(){
+    console.log("red");
+    document.getElementById("demo").innerHTML = "@NadiaAlejandraGomez"
+}
